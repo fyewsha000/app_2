0804e13ab7929f3a13f25e7e7112855212f69aa0
vagrant <vagrant@localhost.localdomain>
2020-05-07 07:32:06 +0000

    [Finish]5-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,9 +2,11 @@ class PostImage < ApplicationRecord
 	belongs_to :user
     attachment :image
     has_many :post_comments, dependent: :destroy
-    has_many :favorite, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    validates :shop_name, presence: true
+    validates :image, presence: true
      def favorited_by?(user)
-     	favorite.where(user_id: user.id).exists?
+     	favorites.where(user_id: user.id).exists?
      end
 
 end
